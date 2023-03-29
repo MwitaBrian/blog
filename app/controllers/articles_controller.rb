01965843beaecb8e54e37ce6a2ff737520afd250
MwitaBrian <brianmwita13@gmail.com>
2023-03-29 17:29:11 +0300

    Implement new and create actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,18 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     # render json: @article
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: unprocessable_entity
+    end
+  end
 end
