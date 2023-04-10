7983756bcc2ad66f24043e1a32bcaa34c186261a
MwitaBrian <brianmwita13@gmail.com>
2023-04-10 15:14:43 +0300

    Add Validations and Display error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
     if @article.save
       redirect_to @article
     else
-      render :new, status: unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
